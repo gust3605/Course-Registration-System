0fc884e4d4a940833f1763609da8267e089eb326
gust3605 <43590327+gust3605@users.noreply.github.com>
2018-12-06 15:43:16 -0600

    pot. query method

--- server.js
+++ server.js
@@ -53,12 +53,24 @@ var class_list = new Vue({
 		results: []
 	},
 	query_db(filter_by) {
+		var query ='SELECT *
+					FROM courses
+					WHERE
+					';
 		let db = new sqlite3.Database('/classes.db', (err) => {
 			if (err) {
 				return console.error(err.message);
 			}
 			console.log('Connected to the in-memory SQlite database');
 		});
+		
+		
+		db.all(query, [], (err, rows) => {
+			if (err) {
+				throw err;
+			}
+			results = rows;
+		}
 	}
 });
 
